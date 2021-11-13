77819990e0f849064a2933e2464f92685cbda8eb
Drew Funderburk <kevinfunderburkbusiness@gmail.com>
2021-11-13 14:56:27 -0600

    11/13/2021 Class

--- Program.cs
+++ Program.cs
@@ -4,53 +4,19 @@ namespace Hello_World
 {
     class Program
     {
-        static void Main(string[] args)
+        // Player stats
+        static string name = "None";
+        static string role = "None";
+        static float health = -1;
+        static float stamina = -1;
+        static float strength = -1;
+        static int level = -1;
+
+        /// <summary>
+        /// Prints the player stats to the console
+        /// </summary>
+        static void PrintPlayerStats()
         {
-            // Player stats
-            string name = "None";
-            string role = "None";
-            float health = -1;
-            float stamina = -1;
-            float strength = -1;
-            int level = -1;
-
-            // Get the player name
-            Console.WriteLine("What is your name?");
-            Console.Write("> ");
-            name = Console.ReadLine();
-
-            // Loop while role is not Knight or Wizard
-            while (role != "Knight" && role != "Wizard")
-            {
-                // Get the player role
-                Console.WriteLine("What is your role?");
-                Console.Write("> ");
-                role = Console.ReadLine();
-
-                // If role is Knight, set our stats and skip the rest
-                if (role == "Knight")
-                {
-                    health = 100;
-                    stamina = 20;
-                    strength = 10;
-                    level = 1;
-                }
-                // If role is Wizard, set our stats and skip the rest
-                else if (role == "Wizard")
-                {
-                    health = 50;
-                    stamina = 10;
-                    strength = 5;
-                    level = 1;
-                }
-                // If our role is none of the above, say invalid input and loop again
-                else
-                {
-                    Console.WriteLine("Invalid input. Choices are Knight or Wizard");
-                }
-            }
-
-            // Print stats
             Console.WriteLine();
             Console.WriteLine("Name:     " + name);
             Console.WriteLine("Role:     " + role);
@@ -58,41 +24,105 @@ namespace Hello_World
             Console.WriteLine("Stamina:  " + stamina);
             Console.WriteLine("Strength: " + strength);
             Console.WriteLine("Level:    " + level);
+        }
 
-            // Goblin
+        /// <summary>
+        /// Prompts the player for input
+        /// </summary>
+        /// <param name="text">Text to display for the prompt</param>
+        /// <returns>Player input</returns>
+        static string Prompt(string text)
+        {
+            Console.WriteLine(text);
+            Console.Write("> ");
+
+            // Return the player's input in the console out of this function
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Ask the player to choose between two options. Loops until input is valid
+        /// </summary>
+        /// <param name="description">Text to display for the prompt</param>
+        /// <param name="option1">First choice</param>
+        /// <param name="option2">Second choice</param>
+        /// <returns>Player's choice</returns>
+        static string PlayerChoice(string description, string option1, string option2)
+        {
+            // Create variable to store choice and initialize it to empty string
+            string choice = "";
+
+            // While choice is not equal to either option1 or option2
+            while (choice != option1 && choice != option2)
+            {
+                // Prompt for input
+                choice = Prompt(description);
+
+                // If choice is not valid, list the choices
+                if (choice != option1 && choice != option2)
+                {
+                    Console.Write("Invalid Input! Choices are: ");
+                    Console.WriteLine(option1 + " or " + option2);
+                }
+            }
+
+            // Return the player's choice out of the function
+            return choice;
+        }
+
+        static void PressAnyKeyToContinue()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
             Console.WriteLine();
-            Console.WriteLine("A wild Goblin appears!");
+        }
+
+        static void GoblinFight()
+        {
+            // Goblin
             float goblinHealth = 10;
             float goblinStamina = 5;
 
             // Goblin fight
             while (health > 0 && goblinHealth > 0)
             {
+                Console.Clear();
+
+                Console.WriteLine();
+                Console.WriteLine("A wild Goblin appears!");
+
+                // Add a line to separate rounds of combat
+                Console.WriteLine("-----------------------------------");
+
+                // Print player stats
+                PrintPlayerStats();
+
+                // Print goblin stats
                 Console.WriteLine();
                 Console.WriteLine("Goblin Health:   " + goblinHealth);
                 Console.WriteLine("Goblin Stamina:  " + goblinStamina);
 
                 string input = "";
-                while (input != "Attack" && input != "Run")
+                // Ask the player to Attack or Run
+                input = PlayerChoice("Would you like to Attack or Run?", "Attack", "Run");
+
+                // Check if the player said Attack or Run
+                if (input == "Attack")
                 {
-                    // Ask the player to Attack or Run
-                    Console.WriteLine("Would you like to Attack or Run?");
-                    Console.Write("> ");
-                    input = Console.ReadLine();
+                    // Damage the goblin
+                    goblinHealth = goblinHealth - 5;
 
-                    // Check if the player said Attack or Run
-                    if (input == "Attack")
-                    {
-                        goblinHealth = goblinHealth - 5;
-                        stamina = stamina - 5;
+                    // Lower player stamina
+                    stamina = stamina - 5;
 
-                        Console.WriteLine("You did 5 damage to the Goblin!");
-                    }
-                    else if (input == "Run")
-                    {
-                        Console.WriteLine("You ran away!");
-                    }
+                    Console.WriteLine("You did 5 damage to the Goblin!");
                 }
+                else if (input == "Run")
+                {
+                    Console.WriteLine("You ran away!");
+                    PressAnyKeyToContinue();
+                }
+
 
                 if (input == "Run")
                 {
@@ -100,9 +130,10 @@ namespace Hello_World
                 }
 
                 // Goblin attack
+
+                // Damage the player
                 health = health - 5;
                 Console.WriteLine("The Goblin did 5 damage");
-                Console.WriteLine("Health: " + health);
 
                 if (goblinHealth <= 0)
                 {
@@ -112,8 +143,47 @@ namespace Hello_World
                 {
                     Console.WriteLine("You died!");
                 }
+
+                PressAnyKeyToContinue();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Get the player name
+            name = Prompt("What is your name?");
+
+            
+            // Get the player role
+            role = PlayerChoice("What is your role?", "Knight", "Wizard");
+
+            // If role is Knight, set our stats and skip the rest
+            if (role == "Knight")
+            {
+                health = 100;
+                stamina = 20;
+                strength = 10;
+                level = 1;
+            }
+            // If role is Wizard, set our stats and skip the rest
+            else if (role == "Wizard")
+            {
+                health = 50;
+                stamina = 10;
+                strength = 5;
+                level = 1;
             }
 
+            // Print stats
+            PrintPlayerStats();
+
+            // Do goblin fight
+            for (int i = 0; i < 3; i++)
+            {
+                GoblinFight();
+            }
+            
 
             // Second fight
         }
